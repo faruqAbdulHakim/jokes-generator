ea0f02fe69b3accd37360ab05d960fcf39c3d0ca
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2021-10-07 20:18:09 +0700

    fix select field bgcolor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,6 +33,7 @@ const SelectField = styled.select`
   padding: 11px 16px;
   margin-right: 16px;
   cursor: pointer;
+  background-color: white;
   box-shadow: inset 0px 0px 4px rgba(0, 0, 0, 0.25);
   position: relative;
   & option {
